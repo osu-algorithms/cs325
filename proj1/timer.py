29985f9599edf353ea80ca69da4f7406121b40ff
Emily <dunhame@onid.orst.edu>
2012-10-11 12:32:56 -0700

    Spaces > tabs

--- proj1/timer.py
+++ proj1/timer.py
@@ -8,22 +8,22 @@ import os
 from time import time
 
 def print2(results, tests, n):
-	print "The tester ran " + str(n) + " tests per algorithm on " + str(len(tests)) + " algorithms."
-	for i in range(0, len(tests)):
-		print "Test " + str(tests[i]) + "\n\tTotal time: " + str(results[i])
-		print "\tTime per execution: " + str(results[i]/n)
+    print "The tester ran " + str(n) + " tests per algorithm on " + str(len(tests)) + " algorithms."
+    for i in range(0, len(tests)):
+        print "Test " + str(tests[i]) + "\n\tTotal time: " + str(results[i])
+        print "\tTime per execution: " + str(results[i]/n)
 
 def main(args, argv):
-	num_tests = int(argv[1])
-	tests = (alg1.alg1, alg2.alg2, alg3.max_sub)
-	results = []
-	testdata = data.get_data(num_tests)
-	for t in tests:
-		start = time()
-		for d in testdata:
-			t(d)
-		results.append(time()-start) 
-	print2(results, tests, num_tests)		
+    num_tests = int(argv[1])
+    tests = (alg1.alg1, alg2.alg2, alg3.max_sub)
+    results = []
+    testdata = data.get_data(num_tests)
+    for t in tests:
+        start = time()
+        for d in testdata:
+            t(d)
+        results.append(time()-start) 
+    print2(results, tests, num_tests)       
 
 if __name__ == "__main__":
-	exit(main(len(sys.argv), sys.argv))
+    exit(main(len(sys.argv), sys.argv))
