942361a69847704f4d2bdca367d32426decaea28
Daniel Thornton <daniel@relud.com>
2012-11-12 03:38:29 +0000

    new algorithm definitely gets up to n=90, but only fills half the arrray to save space. pretty sure it's being killed for memory consumption before we can solve the 11K problem.

--- proj4/d.py
+++ proj4/d.py
@@ -1,11 +1,14 @@
 from distance import distance
 from time import time
 
-n = 100
+n = 90
 m = []
 for i in range(n):
     for j in range(n):
         m.append((i,j))
 t = time()
 distance(m)
+#for i in distance(m):
+#    print i
 print (time() - t)
+
--- proj4/distance.py
+++ proj4/distance.py
@@ -4,14 +4,14 @@ from math import sqrt
 def distance(a):
     d = []
     for i in range(len(a)):
-        d.append(range(len(a)))
+        d.append([])
     for i in range(len(a)):
         x1 = a[i][0]
         y1 = a[i][1]
-        d[i][i] = 0
+        #d[i][i] = 0
         for j in range(i+1,len(a)):
             x2 = a[j][0]
             y2 = a[j][1]
-            d[i][j] = int(sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2)) + 0.5)
-            d[j][i] = d[i][j]
+            d[j].append(int(sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2)) + 0.5))
+            #d[i][j] = d[j][i]
     return d
