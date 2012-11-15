4e4e17518c36a43a83f9a941ba37b4373b03d41f
Daniel Thornton <merthel@gmail.com>
2012-11-14 22:05:10 -0800

    add MST algorithm, but it's still broken

--- proj4/christofides.c
+++ proj4/christofides.c
@@ -7,97 +7,175 @@ typedef struct {
     int y;
 } vector;
 
-int* distance(vector* v, int l){
-    int* e = (int*)malloc(l*l*sizeof(int));
+int* distance(vector* vertices, int length){
+    int* edges = (int*)malloc(length*length*sizeof(int));
     int x1, y1, x2, y2;
+    int distance;
     int i, j, k;
-    int d;
 
-    double s;
-
-    for (i = 0; i < l; i++) {
-        x1 = v[i].x;
-        y1 = v[i].y;
-        for (j = i + 1; j < l; j++) {
-            x2 = v[j].x;
-            y2 = v[j].y;
-            d = (x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2);
-            s = sqrt(d);
-            d = (int) (s + 0.5);
-            e[i*l+j] = d;
-            e[j*l+i] = d;
+    for (i = 0; i < length; i++) {
+        x1 = vertices[i].x;
+        y1 = vertices[i].y;
+        for (j = i + 1; j < length; j++) {
+            x2 = vertices[j].x;
+            y2 = vertices[j].y;
+            distance = (int)(sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2)) + 0.5);
+            edges[i*length+j] = distance;
+            edges[j*length+i] = distance;
         }
     }
-    return e;
+    return edges;
 }
 
-vector* mst(int* e, int l){
-    char* v = (char*)malloc(l*sizeof(char));
-    int* m = (int*)malloc(l*sizeof(int));
-    vector* g = (vector*)malloc((l-1)*sizeof(vector));
-    vector s;
-    int q;
-    
-    int f, i, j, k = 0;
-    v[0] = 1;
-    for (i = 1; i < l; i++) {
-        v[i] = 0;
-        m[i] = 0;
+int* mst(int* edges, int length){
+    // for vertex i, vertices[i] is whether that vertex is
+    // in the current MST
+    char* vertices = (char*)malloc(length*sizeof(char));
+    // for vertex i not in the current MST, minimum[i] is the edge in the
+    // current MST that is closest to i
+    int* minimum = (int*)malloc(length*sizeof(int));
+    // graph is the list of edges in the MST
+    vector* graph = (vector*)malloc((length-1)*sizeof(vector));
+    // alternate form for storing MST
+    int* MST = (int*)malloc((length)*sizeof(int));
+    MST[0] = -1;
+    // shortest is the shortest edge connecting a vertex in the MST to
+    // a vertex not in the MST
+    vector shortest;
+    // shortest_length is the length of shortest
+    int shortest_length;
+    // flag and iterators
+    int flag, i, j, k = 0;
+
+    // intialize arrays
+    vertices[0] = 1;
+    for (i = 1; i < length; i++) {
+        vertices[i] = 0;
+        minimum[i] = 0;
     }
     
-    f = 1;
-    while (f) {
-        f = 0;
-        q = 0;
-        for (i = 0; i < l; i++) {
-            if (v[i]) {
+    flag = 1;
+    while (flag) {
+        flag = 0;
+        for (i = 0; i < length; i++) {
+            if (vertices[i]) {
                 continue;
             }
-            if (!q || (e[m[i]*l+i] < q)) {
-                f = 1;
-                q = e[m[i]*l+i];
-                s.x = m[i];
-                s.y = i;
+            if (!flag || (edges[minimum[i]*length+i] < shortest_length)) {
+                flag = 1;
+                shortest_length = edges[minimum[i]*length+i];
+                shortest.x = minimum[i];
+                shortest.y = i;
             }
         }
-        if (f) {
-            v[s.y] = 1;
-            g[k++] = s; 
+        if (flag) {
+            vertices[shortest.y] = 1;
+            MST[shortest.y] = shortest.x;
+            graph[k++] = shortest; 
 
-            for (i = 0; i < l; i++) {
-                if (!v[i]) {
+            for (i = 0; i < length; i++) {
+                if (!vertices[i]) {
                     continue;
                 }
-                if (e[s.y*l+i] < e[m[i]*l+i]) {
-                    m[i] = s.y;
+                if (edges[shortest.y*length+i] < edges[minimum[i]*length+i]) {
+                    minimum[i] = shortest.y;
                 }
             }
         }
     }
-    free(v);
-    return g;
+    free(vertices);
+    free(minimum);
+    free(graph);
+    return MST;
+}
+
+//returns the minimum perfect matching for odd degree vertices in MST
+vector* perfect_matching(int* MST, int length){
+}
+
+int* mst_tsp(int* edges, int length){
+    char* vertices = (char*)malloc(length*sizeof(char));
+    int* MST = mst(edges,length);
+    int* solution = (int*)malloc((length+2)*sizeof(int));
+    int v1 = 0, v2 = 0;
+    int flag, i, j = 1;
+    
+    // intialize array
+    vertices[0] = 1;
+    for (i = 1; i < length; i++) {
+        vertices[i] = 0;
+    }
+
+    solution[0] = 0; //current length
+    solution[1] = 0; //first city
+    
+    while (v2 > -1) {
+        flag = 0;
+        for (i = 1; i < length - 1; i++) {
+            if ( vertices[i] ) { continue; }
+            if (MST[i] == v2) {
+                flag = 1;
+                solution[++j] = i;
+                solution[0] += edges[v1*length+i];
+                v1 = i;
+                v2 = i;
+                vertices[i] = 1;
+                break;
+            }
+        }
+        if (!flag) {
+            v2 = MST[v2];
+        }
+    }
+    solution[++j] = 0;
+    solution[0] += edges[v1*length+i];
+
+    printf("%d\n", j);
+    free(MST);
+    return solution;
+}
+
+int* christofides(int* edges, int length){
+    int* MST = mst(edges,length);
+    vector* PM = perfect_matching(MST, length-1);
+    int* solution = (int*)malloc((length+2)*sizeof(int));
+    
+    solution[0] = 0;
+    
+    free(MST);
+    free(PM);
+    return solution;
 }
 
 int main(){
-    int n = 125;
-    int l = n*n;
-    vector* v = (vector*)malloc(l*sizeof(vector));
-    vector* g;
-    int* e;
+    int n = 4;
+    int length = n*n;
+    vector* vertices = (vector*)malloc(length*sizeof(vector));
+    int* solution_mst;
+    int* solution_christofides;
+    int* edges;
     int i, j, k = 0;
     
-
+    // create a TSP input that is every point in a nxn matrix
     for (i = 0; i < n; i++){
         for (j = 0; j < n; j++){
-            v[k].x = i;
-            v[k++].y = j;
+            vertices[k].x = i;
+            vertices[k++].y = j;
         }
     }
 
-    e = distance(v,l);
-    g = mst(e,l);
+    edges = distance(vertices,length);
+    printf("edges calculated\n");
+    solution_mst = mst_tsp(edges,length);
+    printf("length of MST algorithm path: %d\n", solution_mst[0]);
+    for (i = 1; i < length + 2; i++) {
+        printf("City %d: %d ; trip :%d\n", i, solution_mst[i], edges[solution_mst[i]*length+solution_mst[i-1]]);
+    }
+    //solution_christofides = christofides(edges,length);
+    //printf("length of Christofides algorithm path: %d\n", solution_christofides[0]);
 
-    //printf("cake");
-    free(e);
-    free(v);
+    free(vertices);
+    free(edges);
+    free(solution_mst);
+    //free(solution_christofides);
 }
