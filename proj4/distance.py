84ff505b9a2523ca9ada53916b8a3c1ff9819b45
Daniel Thornton <merthel@gmail.com>
2012-11-14 16:12:16 -0800

    revert distance.py to the algorithm that does explode

--- proj4/distance.py
+++ proj4/distance.py
@@ -2,16 +2,18 @@
 from math import sqrt
 
 def distance(a):
-    length = len(a)
-    d = eval(`[[0]*length]*length`)
-    for i in range(length):
+    d = []
+    for i in range(len(a)):
+        d.append([])
+    for i in range(len(a)):
         x1 = a[i][0]
         y1 = a[i][1]
-        for j in range(i+1,length):
+        #d[i][i] = 0
+        for j in range(i+1,len(a)):
             x2 = a[j][0]
             y2 = a[j][1]
-            d[j][i] = int(sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2)) + 0.5)
-            d[i][j] = d[j][i]
+            d[j].append(int(sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2)) + 0.5))
+            #d[i][j] = d[j][i]
     return d
 
 def mirror(a):
