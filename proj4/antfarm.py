93d792277b26c6c64d52b3bb24e6e7170cbcdd80
unknown <casejae@onid.orst.edu>
2012-11-16 18:56:27 -0800

    Psuedo code for an ant colony optimization algorithm

---
+++ proj4/antfarm.py
@@ -0,0 +1,31 @@
+#antfarm - ant colony optimization psuedocode
+
+def ant
+#while(not home)
+	#choose next point
+		#nearest?
+		#random?
+		#scent?
+		
+def markpaths
+#get lastest antrail
+
+#rate trail for timelyness
+
+#fade old trails
+
+
+def main
+#read imput from text
+
+#set number of ants, trials
+
+#for(each ant)
+	#traverse graph
+	#mark path
+	#display best path so far
+
+
+
+
+
